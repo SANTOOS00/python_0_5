3b7374708ce0928a6da71e91e8d0de0c73e43cc4
SANTOOS00 <mohamederraiss@icloud.com>
2025-12-29 08:27:41 +0100

    is finich

--- ex06/ft_garden_summary.py
+++ ex06/ft_garden_summary.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_summary.py                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/12/29 08:25:52 by moerrais          #+#    #+#              #
+#    Updated: 2025/12/29 08:25:53 by moerrais         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 def ft_garden_summary():
     garden_name = str(input("Enter garden name: "))
     number_plants = int(input("Enter number of plants: "))
