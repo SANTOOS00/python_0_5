8b9713b431417cdb612f2ae497efe56b4842950f
SANTOOS00 <mohamederraiss@icloud.com>
2025-12-31 16:00:20 +0100

    no finich :)

--- py_01/ex0/ft_garden_intro.py
+++ py_01/ex0/ft_garden_intro.py
@@ -6,16 +6,22 @@
 #    By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/30 20:18:29 by moerrais          #+#    #+#              #
-#    Updated: 2025/12/30 20:19:06 by moerrais         ###   ########.fr        #
+#    Updated: 2025/12/31 15:55:39 by moerrais         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 Plant = "Rose"
 Height = 25
 Age = 30
+
+
+def ft_garden_intro():
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {Plant}")
+    print(f"Height: {Height}cm")
+    print(f"Age: {Age} days")
+    print("\n=== End of Program ===")
+
+
 if __name__ == "__main__":
-	print ("=== Welcome to My Garden ===")
-	print (f"Plant: {Plant}")
-	print (f"Height: {Height}cm")
-	print (f"Age: {Age} days")
-	print ("\n=== End of Program ===")
+    ft_garden_intro()
---
+++ test_python/test_00/test_1.py
@@ -0,0 +1,8 @@
+
+
+x = [2 , 3]
+y = x;
+
+y += [4, 5, 6]
+print (x, y)
+print(id(x) == id(y))
