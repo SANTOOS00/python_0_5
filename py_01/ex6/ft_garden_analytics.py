5d9a4d8dbe87db98d6a879a675b16da6233a74a8
Kh4wf0x <mohamederraiss@icloud.com>
2026-01-10 20:47:28 +0200

    ss

--- py_01/ex6/ft_garden_analytics.py
+++ py_01/ex6/ft_garden_analytics.py
@@ -4,6 +4,10 @@ class Plant:
         self.name = name
         self.height = height
 
+    def add_height(self):
+        self.height += 1
+        print(f"{self.name} grew 1cm")
+
 
 class FloweringPlant(Plant):
     def __init__(self, name, height, color):
@@ -18,16 +22,32 @@ class PrizeFlower(FloweringPlant):
 
 
 class GardenManager:
-    def __init__(self, name_garden):
-        self.name_garden = name_garden
+    def __init__(self, owner):
+        self.owner = owner
         self.Plants = []
 
     def add_Plant(self, plant):
         self.Plants.append(plant)
-        print(f"Added {plant.name} to {self.name_garden} garden")
+        print(f"Added {plant.name} to {self.owner}'s garden")
+
+    def All_Plants_Grow(self):
+        print(f"{self.owner} is helping all plants grow...")
+        for Plant in self.Plants:
+            Plant.add_height()
+
+
+def main():
+    print("=== Garden Management System Demo ===")
+    print()
+    my_garden1 = GardenManager("Alice")
+    my_garden2 = GardenManager("hamid")
+    my_garden1.add_Plant(Plant("Oak Tree", 100))
+    my_garden2.add_Plant(Plant("Oak Tree", 100))
+    my_garden1.add_Plant(FloweringPlant("Rose", 25, "red"))
+    my_garden1.add_Plant(PrizeFlower("Sunflower", 50, "Yellow", 10))
+    print()
+    my_garden1.All_Plants_Grow()
 
 
 if __name__ == "__main__":
-    my_garden = GardenManager("Alice's")
-    my_garden.add_Plant(Plant("Oak Tree", 25))
-    my_garden.add_Plant(FloweringPlant("Rose", 25, "red"))
+    main()
